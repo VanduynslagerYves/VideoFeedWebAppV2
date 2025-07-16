afbdfee9794f8d323695d6e0e411cc10622207bb
VanduynslagerYves <25350116+VanduynslagerYves@users.noreply.github.com>
2025-07-16 18:35:16 +0200

    moved js to component file

---
+++ VideoFeed/wwwroot/js/videoFeedComponent.js
@@ -0,0 +1,35 @@
+﻿const app = Vue.createApp({});
+
+app.component('video-feed', {
+    props: {
+        cameraId: String,
+        hubUrl: String
+    },
+    template: `
+        <div>
+            <h3>Camera: {{ cameraId }}</h3>
+            <canvas :ref="cameraId" width="800" height="600"></canvas>
+        </div>
+    `,
+    mounted() {
+        const canvas = this.$refs[this.cameraId];
+        const ctx = canvas.getContext('2d');
+
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl(this.hubUrl)
+            .build();
+
+        connection.on("ReceiveFrame", data => {
+            const img = new Image();
+            img.onload = () => {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.drawImage(img, 0, 0);
+            };
+            img.src = "data:image/jpeg;base64," + data;
+        });
+
+        connection.start().catch(err => console.error("SignalR Error:", err.toString()));
+    }
+});
+
+app.mount("#app");
