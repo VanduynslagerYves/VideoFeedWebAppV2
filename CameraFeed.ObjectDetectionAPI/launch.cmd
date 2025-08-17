b70c8afd464f67aee9b6734e4a242d0c98627ac6
VanduynslagerYves <25350116+VanduynslagerYves@users.noreply.github.com>
2025-08-18 00:58:13 +0200

    TensorRT model

--- CameraFeed.ObjectDetectionAPI/launch.cmd
+++ CameraFeed.ObjectDetectionAPI/launch.cmd
@@ -1,2 +1,3 @@
-python -m uvicorn app:app --reload --host 127.0.0.1 --port 8000
+::python -m uvicorn app:app --reload --host 127.0.0.1 --port 8000
+py -3.10 -m uvicorn app:app --reload --host 127.0.0.1 --port 8000
 :: python app.py
---
+++ CameraFeed.ObjectDetectionAPI/util/create_tensor_model.py
@@ -0,0 +1,21 @@
+#use this script to create a TensorRT model from a YOLOv8 model (Run this with Python 3.10 only,
+# TensorRT required ONNX and is only supported on 3.10 at the moment of writing)
+
+# Ensure you have the required packages installed:
+# ultralytics with torch enabled for gpu
+# tensorrt
+
+from ultralytics import YOLO
+import tensorrt
+print(tensorrt.__version__)
+
+model = YOLO("yolov8n.pt")
+# Export to TensorRT engine
+model.export(
+    format="engine",
+    device=0,       # device=0 for GPU, device=cpu for CPU
+    #imgsz=416,      # smaller input size
+    half=True,      # use FP16 (half-precision)
+    dynamic=True,  # set True if you want dynamic shapes
+    simplify=True   # simplify ONNX graph before TensorRT conversion
+)
