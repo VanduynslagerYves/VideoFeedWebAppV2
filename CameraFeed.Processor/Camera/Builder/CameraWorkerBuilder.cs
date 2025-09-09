a2a996d96462482bbc15b1ca917c0e06291fa863
VanduynslagerYves <25350116+VanduynslagerYves@users.noreply.github.com>
2025-09-09 06:59:25 +0200

    comment out CameraWorkerBuilder for later implementation/refactoring

--- CameraFeed.Processor/Camera/Builder/CameraWorkerBuilder.cs
+++ CameraFeed.Processor/Camera/Builder/CameraWorkerBuilder.cs
@@ -1,72 +1,72 @@
-﻿using CameraFeed.Processor.Camera.Worker;
-using CameraFeed.Processor.Services.gRPC;
-using Microsoft.AspNetCore.SignalR;
+﻿//using CameraFeed.Processor.Camera.Worker;
+//using CameraFeed.Processor.Services.gRPC;
+//using Microsoft.AspNetCore.SignalR;
 
-namespace CameraFeed.Processor.Camera.Builder;
+//namespace CameraFeed.Processor.Camera.Builder;
 
-public class CameraWorkerBuilder(
-    ILogger<CameraWorker> logger,
-    IHubContext<CameraHub> hubContext,
-    IVideoCaptureFactory videoCaptureFactory,
-    IBackgroundSubtractorFactory backgroundSubtractorFactory,
-    IObjectDetectionGrpcClient objectDetectionClient)
-{
-    private int _cameraId;
-    private bool _useMotionDetection;
-    private CameraResolution _resolution = new() { Width = 1920, Height = 1080 };
-    private int _framerate = 30;
+//public class CameraWorkerBuilder(
+//    ILogger<CameraWorker> logger,
+//    IHubContext<CameraHub> hubContext,
+//    IVideoCaptureFactory videoCaptureFactory,
+//    IBackgroundSubtractorFactory backgroundSubtractorFactory,
+//    IObjectDetectionGrpcClient objectDetectionClient)
+//{
+//    private int _cameraId;
+//    private bool _useMotionDetection;
+//    private CameraResolution _resolution = new() { Width = 1920, Height = 1080 };
+//    private int _framerate = 30;
 
-    public CameraWorkerBuilder WithCameraId(int cameraId)
-    {
-        _cameraId = cameraId;
-        return this;
-    }
+//    public CameraWorkerBuilder WithCameraId(int cameraId)
+//    {
+//        _cameraId = cameraId;
+//        return this;
+//    }
 
-    public CameraWorkerBuilder WithResolution(int width, int height)
-    {
-        _resolution = new CameraResolution { Width = width, Height = height };
-        return this;
-    }
+//    public CameraWorkerBuilder WithResolution(int width, int height)
+//    {
+//        _resolution = new CameraResolution { Width = width, Height = height };
+//        return this;
+//    }
 
-    public CameraWorkerBuilder WithFramerate(int framerate)
-    {
-        _framerate = framerate;
-        return this;
-    }
+//    public CameraWorkerBuilder WithFramerate(int framerate)
+//    {
+//        _framerate = framerate;
+//        return this;
+//    }
 
-    public CameraWorkerBuilder UseMotionDetection(bool value = true)
-    {
-        _useMotionDetection = value;
-        return this;
-    }
+//    public CameraWorkerBuilder UseMotionDetection(bool value = true)
+//    {
+//        _useMotionDetection = value;
+//        return this;
+//    }
 
-    public Task<ICameraWorker> Build()
-    {
-        var options = new WorkerOptions
-        {
-            CameraId = _cameraId,
-            CameraName = $"Camera {_cameraId}",
-            Mode = InferenceMode.MotionBased,
-            CameraOptions = new CameraOptions
-            {
-                Resolution = _resolution,
-                Framerate = _framerate
-            },
-            MotionDetectionOptions = new MotionDetectionOptions
-            {
-                DownscaleFactor = 16,
-                MotionRatio = 0.01,
-            }
-        };
+//    public Task<ICameraWorker> Build()
+//    {
+//        var options = new WorkerOptions
+//        {
+//            CameraId = _cameraId,
+//            CameraName = $"Camera {_cameraId}",
+//            Mode = InferenceMode.MotionBased,
+//            CameraOptions = new CameraOptions
+//            {
+//                Resolution = _resolution,
+//                Framerate = _framerate
+//            },
+//            MotionDetectionOptions = new MotionDetectionOptions
+//            {
+//                DownscaleFactor = 16,
+//                MotionRatio = 0.01,
+//            }
+//        };
         
-        var worker = new CameraWorker(
-            options,
-            videoCaptureFactory,
-            backgroundSubtractorFactory,
-            objectDetectionClient,
-            logger,
-            hubContext);
+//        var worker = new CameraWorker(
+//            options,
+//            videoCaptureFactory,
+//            backgroundSubtractorFactory,
+//            objectDetectionClient,
+//            logger,
+//            hubContext);
 
-        return Task.FromResult<ICameraWorker>(worker);
-    }
-}
+//        return Task.FromResult<ICameraWorker>(worker);
+//    }
+//}
